b3baf15e18b67471e99b0493cb07bb47d6432286
wmckay555 <67658851+wmckay555@users.noreply.github.com>
2020-09-16 17:55:23 -0700

    music plays in the game

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicManager : MonoBehaviour
+{
+
+    AudioSource music;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        music = GetComponent<AudioSource>();
+        music.loop = true;
+        music.Play();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
