ec1d707d8e12c37deaec317266b9c66e4daa6b1e
Philip Greene <philip_greene@hotmail.com>
2020-09-22 23:15:29 +0100

    Updated logo - Linked TitleScreen scene to test - Wrote script to scale objects based on music loudness

---
+++ Assets/Scripts/AudioVisuals/AudioVisScale.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioVisScale : MonoBehaviour
+{
+    public AudioSource audioSource;
+    private GameObject objectToVisualizeAudio;
+    public float loudnessMultiplier = 1;
+    public float currentLoudness;
+    public float sampleSum;
+
+    private float[] audioSamples = new float[1024];
+    private Vector3 origObjectScale;
+
+    private void Awake()
+    {
+        objectToVisualizeAudio = this.gameObject;
+        origObjectScale = objectToVisualizeAudio.transform.localScale;
+    }
+
+    private void FixedUpdate()
+    {
+        sampleSum = 0;
+        currentLoudness = 0;        
+
+        // Fill array audioSamples with number of samples starting from current playback position of audioSource
+        audioSource.clip.GetData(audioSamples, audioSource.timeSamples);
+
+        foreach (float sample in audioSamples)
+        {
+            sampleSum += Mathf.Abs(sample); // The samples are floats ranging from -1.0f to 1.0f so Abs to ignore the sign(+-) of this value 
+        }
+
+        currentLoudness = sampleSum / audioSamples.Length; // Average loudness
+
+        ScaleObject(currentLoudness);
+    }
+
+    private void ScaleObject(float loudness)
+    {
+        // Scale in all directions
+        Vector3 scaleToVector = new Vector3(origObjectScale.x + loudness * loudnessMultiplier, origObjectScale.y + loudness * loudnessMultiplier, origObjectScale.z + loudness * loudnessMultiplier);
+
+        objectToVisualizeAudio.transform.localScale = Vector3.Lerp(objectToVisualizeAudio.transform.localScale, scaleToVector, Time.deltaTime);
+    }
+}
