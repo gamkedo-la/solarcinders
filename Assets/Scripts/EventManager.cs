e9f73974600ddd14b11b6750e26a9545bf3fdd3a
wmckay555 <67658851+wmckay555@users.noreply.github.com>
2020-09-22 16:40:58 -0700

    Started working on event manager

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class EventManager : MonoBehaviour
+{
+
+  
+
+    public Event[] events;
+
+    int i = 0;
+
+    float countUp = 0;
+
+    public int Bookmark;
+
+    public int Shift;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = 0;
+        countUp = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countUp += Time.deltaTime;
+
+        if (events[i].spawnTime >= countUp)
+        {
+            
+            Instantiate(events[i].E, gameObject.transform.position, Quaternion.identity, gameObject.transform);
+                     
+            i++;
+        }
+    }
+
+    public void ShiftEvents()
+    {
+
+        for(int j = Bookmark; j < events.Length; j++)
+        {
+            events[j].spawnTime += Shift;
+
+        }
+
+    }
+
+
+    public void SortByTime()
+    {
+
+
+    }
+
+}
+
+[System.Serializable]
+public struct Event
+{
+    public GameObject E;
+
+    public int spawnTime;
+
+}
