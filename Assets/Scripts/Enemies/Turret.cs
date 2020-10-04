2d2c9a186f1a21ffca8d6ec20bc5cd4e688d2b52
Philip Greene <philip_greene@hotmail.com>
2020-10-04 16:20:43 +0100

    Created Turret enemy prefab - Added to Event Manager in test scene

--- Assets/Scripts/Enemies/Turret.cs
+++ Assets/Scripts/Enemies/Turret.cs
@@ -2,17 +2,103 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlybyTurret : MonoBehaviour
+public class Turret : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform turretWeapon;
+    public Transform target;
+
+    public int Health;
+    public int PointsGiven;
+    public AudioSource explodeSFX;
+
+    float ShotTimer = 0f;
+    public const float ShotReset = 1.0f;
+
+    public float turretSpeed;
+
+    private EnemyBase myEnemyBase;
+    private Rigidbody myRigidbody;
+
+    private void Start()
     {
-        
+        myEnemyBase = GetComponent<EnemyBase>();
+        myRigidbody = GetComponent<Rigidbody>();
+        target = GameObject.Find("Ship").transform;
+        turretSpeed = GameObject.Find("Terrain Manager").GetComponent<TerrainManager>().terrainSpeed;
+        ResetShotTimer();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        if (target != null)
+        {
+            turretWeapon.LookAt(target);
+        }
         
+        if (myEnemyBase.Active)
+        {
+            myRigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+            transform.Translate(-Vector3.forward * turretSpeed * Time.deltaTime);
+        }
+        else
+        {
+            myRigidbody.constraints = RigidbodyConstraints.FreezeAll;
+        }
+        
+        if (transform.position.z < 0)
+        {
+            Despawn();
+        }
+    }
+
+    private void Update()
+    {
+        ShotTimer -= Time.deltaTime;
+
+        if (ShotTimer <= 0)
+        {
+            gameObject.GetComponent<Shoot>().Fire();
+            ResetShotTimer();
+        }
+    }
+
+    private void ResetShotTimer()
+    {
+        ShotTimer = ShotReset;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "PlayerLaser")
+        {
+
+            TakeDamage(collision.gameObject.GetComponent<Damage>().damage);
+        }
+    }
+
+    private void TakeDamage(int damage)
+    {
+        Health -= damage;
+        if (Health <= 0)
+        {
+            StartCoroutine("TurretDeathRoutine");
+        }
+    }
+
+    private void Despawn()
+    {
+        Destroy(gameObject);
+    }
+
+    IEnumerator TurretDeathRoutine()
+    {
+        gameObject.transform.position = new Vector3(10000, 10000, 10000);
+        GameObject player = GameObject.Find("Ship");
+        player.GetComponent<Combo>().Add();
+        player.GetComponent<Score>().ScoreChange(PointsGiven);
+        player.GetComponent<PowerUp>().Add(GetComponent<Charge>().charge);
+        explodeSFX.Play();
+        yield return new WaitForSeconds(explodeSFX.clip.length);
+        Destroy(gameObject);
     }
 }
