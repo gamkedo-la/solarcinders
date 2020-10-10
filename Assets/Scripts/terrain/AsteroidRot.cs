46e644af04b63c5a1b211e6d32a77dea17807e38
wmckay555 <67658851+wmckay555@users.noreply.github.com>
2020-10-09 23:34:17 -0700

    Asteroid model

--- Assets/Scripts/terrain/AsteroidRot.cs
+++ Assets/Scripts/terrain/AsteroidRot.cs
@@ -12,7 +12,9 @@ public class AsteroidRot : MonoBehaviour
     {
         Rot = new Vector3(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
 
-        transform.rotation.eulerAngles.Set(Rot.x, Rot.y, Rot.z);
+        transform.rotation = Random.rotation;
+
+        transform.localScale = new Vector3(Random.Range(1.0f, 2.0f), Random.Range(1.0f, 2.0f), Random.Range(1.0f, 2.0f));
     }
 
     // Update is called once per frame
