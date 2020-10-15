4ad319d58ed28d0e9fedb25cb5aa09a795184c48
wmckay555 <67658851+wmckay555@users.noreply.github.com>
2020-10-14 19:33:57 -0700

    laser wall and tower hazards

--- Assets/Scripts/LaserWall.cs
+++ Assets/Scripts/LaserWall.cs
@@ -2,19 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AsteroidMovement : MonoBehaviour
+public class LaserWall : MonoBehaviour
 {
     public float speed = 1.0f;
-
-
-
-
     
+
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        speed = GameObject.Find("Terrain Manager").GetComponent<TerrainManager>().terrainSpeed * -1;
 
     }
 
@@ -26,22 +27,18 @@ public class AsteroidMovement : MonoBehaviour
             transform.position += Vector3.forward * speed * Time.deltaTime;
         }
 
-        if(transform.position.z < 0)
+        if (transform.position.z < 0)
         {
 
             Despawn();
         }
     }
 
-    
+
 
     void Despawn()
     {
 
         Destroy(gameObject);
     }
-
-    
-
-    
 }
