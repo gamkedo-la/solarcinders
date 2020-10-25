172995f63b6647b50f63cfdabcfb7a64e83a90df
wmckay555 <67658851+wmckay555@users.noreply.github.com>
2020-10-24 21:10:18 -0700

    bossfight music

---
+++ Assets/Scripts/MusicManagerDelux.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManagerDelux : MonoBehaviour
+{
+
+    public AudioSource intro;
+    public AudioSource loop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        intro.Play();
+        loop.loop = true;
+        loop.PlayDelayed(intro.clip.length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
