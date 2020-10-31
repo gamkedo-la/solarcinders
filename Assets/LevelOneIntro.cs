74b8ba3b762bf1d4a5f9345ba31c54c93b6ab2ea
wmckay555 <67658851+wmckay555@users.noreply.github.com>
2020-10-30 21:15:33 -0700

    progress on level 1 intro animation, background details on level 3

---
+++ Assets/LevelOneIntro.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelOneIntro : MonoBehaviour
+{
+
+    public GameObject[] lights;
+
+    public float lightTimer;
+    public float lightReset;
+
+    public float t;
+
+    public float y;
+
+    public float tt;
+
+    Vector3 temp = new Vector3(0, 0, 0);
+
+    float speed;
+
+    int LT = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        temp = transform.position;
+
+        y = temp.y;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (t <= 1)
+        {
+            temp.y = Mathf.Lerp(0, -2, t);
+            t += Time.deltaTime;
+
+            transform.position = temp;
+        }
+        if (t > 1)
+        {
+
+            speed = Mathf.Lerp(0, 600, tt);
+
+            tt += (Time.deltaTime / 5);
+
+            temp.z -= speed * Time.deltaTime;
+
+            transform.position = temp;
+
+
+        }
+
+        lightTimer -= Time.deltaTime;
+
+        if(lightTimer <= 0 && LT < 5)
+        {
+            lights[LT].SetActive(true);
+            LT++;
+
+            lightReset *= 0.84f ;
+            lightTimer = lightReset;
+            
+
+        }
+
+        if(transform.position.z < -200)
+        {
+
+            Destroy(gameObject);
+        }
+
+
+
+
+    }
+}
