15e2838b5fe833ae87c38b50d597e8cbb3e61038
wmckay555 <67658851+wmckay555@users.noreply.github.com>
2020-10-30 17:42:20 -0700

    level 2 background details, started working on level 1 intro sequence

---
+++ Assets/MountainSpawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MountainSpawner : MonoBehaviour
+{
+
+    public GameObject[] mountains;
+
+
+    public float timer = 0;
+
+    public float resetMin;
+    public float resetMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        timer = Random.Range(resetMin, resetMax);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (timer <= 0)
+        {
+
+            Vector3 spawnPosition = gameObject.transform.position;
+
+
+
+
+
+            Instantiate(mountains[Random.Range(0,mountains.Length)], spawnPosition, transform.rotation);
+            timer = Random.Range(resetMin, resetMax);
+
+
+        }
+        else
+        {
+
+            timer -= Time.deltaTime;
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/terrain/Mountain.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mountain : MonoBehaviour
+{
+
+    Vector3 temp = new Vector3(0, 0, 0);
+
+    Vector3 rot = new Vector3(0, 0, 0);
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rot.y = Random.Range(0, 4) * 90;
+
+        transform.rotation = Quaternion.Euler(rot);
+
+        speed = GameObject.Find("Terrain Manager").GetComponent<TerrainManager>().terrainSpeed;
+
+        temp = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (GameObject.FindGameObjectWithTag("PauseMenu").GetComponent<PauseMenu>().isPaused == false)
+        {
+            temp.z -= speed * Time.deltaTime;
+            transform.position = temp;
+        }
+
+        if (transform.position.z < -150)
+        {
+
+            Destroy(gameObject);
+        }
+
+    }
+}
