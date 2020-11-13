01333cd11969bf58630f95aa8ce130d61e371715
wmckay555 <67658851+wmckay555@users.noreply.github.com>
2020-11-12 18:09:02 -0800

    updated button sprites, resetting controls now turns off mouse control

--- Assets/Scripts/Input/ResetBindings.cs
+++ Assets/Scripts/Input/ResetBindings.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class ResetBindings : MonoBehaviour
 {
@@ -12,6 +13,9 @@ public class ResetBindings : MonoBehaviour
     public InputActionAsset playerInputActions;
     private InputActionMap playerActionMap;
 
+    public GameObject MouseOn;
+    public GameObject MouseOff;
+
     private void Start()
     {
         playerActionMap = playerInputActions.FindActionMap("Player");
@@ -20,6 +24,11 @@ public class ResetBindings : MonoBehaviour
     public void ResetAllBindings()
     {
         playerActionMap.RemoveAllBindingOverrides();
+
+        PlayerPrefs.SetInt("MouseControl", -1);
+
+        MouseOff.GetComponent<Button>().interactable = true;
+        MouseOn.GetComponent<Button>().interactable = false;
 
         if (OnReset != null)
         {
