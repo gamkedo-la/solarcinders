58e93a8fae61eabd15b327dfe355b1a228ccf0f3
wmckay555 <67658851+wmckay555@users.noreply.github.com>
2020-11-12 16:41:20 -0800

    Buttons for activating and deactivating mouse control

---
+++ Assets/Scripts/MouseConButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseConButton : MonoBehaviour
+{
+
+    public GameObject other;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void Switch()
+    {
+
+        PlayerPrefs.SetInt("MouseControl", PlayerPrefs.GetInt("MouseControl") * - 1);
+
+        other.GetComponent<Button>().interactable = true;
+
+        gameObject.GetComponent<Button>().interactable = false;
+
+    }
+}
--- Assets/Scripts/MouseControlInit.cs
+++ Assets/Scripts/MouseControlInit.cs
@@ -2,21 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseControl : MonoBehaviour
+public class MouseControlInit : MonoBehaviour
 {
-
-    public bool mouseCon;
-
     // Start is called before the first frame update
     void Start()
     {
-        
-        if(PlayerPrefs.GetInt("MouseControl") == 1)
-        {
-
-            mouseCon = true;
-        }
-
+        PlayerPrefs.SetInt("MouseControl", -1);
     }
 
     // Update is called once per frame
