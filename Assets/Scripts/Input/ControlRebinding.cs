35374cc9d7b722bf08e99e71a8396f737bb2bcaa
Philip Greene <philip_greene@hotmail.com>
2020-11-15 02:45:12 +0000

    Added extra buttons for changing alternate keybinds for same action - Attempting to bind same key twice to single action is now cancelled - Warning popup if same key is used on different actions

--- Assets/Scripts/Input/ControlRebinding.cs
+++ Assets/Scripts/Input/ControlRebinding.cs
@@ -13,20 +13,27 @@ public class ControlRebinding : MonoBehaviour
 
     private InputActionMap playerActionMap;
 
+    public GameObject DuplicateBindingWarningPanel;
+
+    public delegate void UnbindAction();
+    public static event UnbindAction OnUnbind;
+
     private void OnEnable()
     {
         ResetBindings.OnReset += SetBindingText;
+        ControlRebinding.OnUnbind += SetBindingText;
     }
 
     private void OnDisable()
     {
         ResetBindings.OnReset -= SetBindingText;
+        ControlRebinding.OnUnbind -= SetBindingText;
     }
 
     private void Start()
     {
         playerActionMap = playerInputActions.FindActionMap("Player");
-        SetBindingText();        
+        SetBindingText();
     }
 
     public void PerformRebinding()
@@ -39,7 +46,7 @@ public class ControlRebinding : MonoBehaviour
     {
         var rebindOperation = actionToRebind.PerformInteractiveRebinding(bindingsIndex)
                     // To avoid accidental input from mouse motion
-                    //.WithControlsExcluding("Mouse")
+                    //.WithControlsExcluding("Mouse")            
                     .OnMatchWaitForAnother(0.1f)
                     .Start();
 
@@ -49,9 +56,50 @@ public class ControlRebinding : MonoBehaviour
            {
                Debug.Log($"Rebound '{actionToRebind}' to '{operation.selectedControl}'");
                operation.Dispose();
+               UnbindDuplicates();
                SetBindingText();
                playerActionMap.Enable();
            });
+    }
+
+    private void UnbindDuplicates()
+    {
+        foreach (InputActionMap inputActionMap in playerInputActions.actionMaps)
+        {
+            foreach (var action in inputActionMap.actions)
+            {
+                //Debug.Log(action.name + actionReference.action.name);
+                if (action.name != actionReference.action.name)
+                {
+                    for (int i = 0; i < action.bindings.Count; i++)
+                    {
+                            if (action.bindings[i].effectivePath == actionReference.action.bindings[bindingsIndex].effectivePath)
+                            {
+                                Debug.Log("Duplicate Detected In Other Input Map " + action.bindings[i].effectivePath);
+                                DuplicateBindingWarningPanel.SetActive(true);
+                            }
+                    }
+                }
+            }
+        }
+
+        Debug.Log(actionReference.action.bindings[bindingsIndex].effectivePath);
+        for (int i = 0; i < actionReference.action.bindings.Count; i++)
+        {
+            if (i != bindingsIndex)
+            {
+                if (actionReference.action.bindings[i].effectivePath == actionReference.action.bindings[bindingsIndex].effectivePath)
+                {
+                    Debug.Log("Duplicate Detected " + actionReference.action.bindings[i].effectivePath);
+                    actionReference.action.RemoveBindingOverride(i);
+                    actionReference.action.RemoveBindingOverride(bindingsIndex);
+                    if (OnUnbind != null)
+                    {
+                        OnUnbind();
+                    }
+                }
+            }
+        }
     }
 
     private void SetBindingText()
