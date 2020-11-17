b812b6b1c3808ff004cd7addae5c6f202fb434d8
wmckay555 <67658851+wmckay555@users.noreply.github.com>
2020-11-16 16:30:59 -0800

    worked on vfx for engine

---
+++ Assets/Scripts/player/EngineEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineEffect : MonoBehaviour
+{
+
+
+    public float scrollSpeed = 50.0f;
+    //spublic string thisTexture;
+    public MeshRenderer rend;
+
+    public Vector2 OS;
+
+    void Start()
+    {
+        rend = GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        float offset = Time.time * scrollSpeed;
+        rend.material.SetTextureOffset(0, new Vector2(offset, offset));
+        OS = rend.material.GetTextureOffset(0);
+    }
+}
